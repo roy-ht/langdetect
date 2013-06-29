a638e075ccc0998a001c60deee3daf85db9b91d5
Hiroyuki Tanaka <aflc0x@gmail.com>
2013-06-29 16:26:11 +0900

    Refactor: file names

---
+++ src/command.cpp
@@ -0,0 +1,32 @@
+// langdetect コマンドのエントリポイント
+
+#include <iostream>
+#include <fstream>
+#include "./langdetect.h"
+
+using langdetect::Detector;
+using std::ifstream;
+using std::string;
+
+size_t const MAX_READ_SIZE = 4096;
+
+void message(char const *path, char const *msg) {
+    std::cout << path << ": " << msg << std::endl;
+}
+
+int main(int argc, char const *argv[]) {
+    Detector detector;
+    for(int i = 1; i < argc; ++i) {
+        char const *path = argv[i];
+        ifstream ifs(path);
+        if(!ifs.is_open()) {
+            message(path, "failed to open");
+        } else {
+            char buf[MAX_READ_SIZE];  // read max 4k bytes
+            ifs.read(buf, MAX_READ_SIZE);
+            std::streamsize readcount = ifs.gcount();
+            langdetect::Detected result = detector.detect(buf, readcount);
+            message(path, result.name().c_str());
+        }
+    }
+}
--- src/langdetect.cpp
+++ src/langdetect.cpp
@@ -1,39 +1,157 @@
-// langdetect コマンドのエントリポイント
-
+#include <string>
+#include <map>
+#include <random>
+#include <algorithm>
 #include <iostream>
-#include <fstream>
-#include "./detector.h"
+#include <iomanip>
 
-using langdetect::Detector;
-using std::ifstream;
+#include "./langdetect.h"
+#include "./ngram_storage.h"
+#include "./code_sequence.h"
+
+#define LANG_FAIL_EMPTY Detected("empty", 0.0)
+
 using std::string;
+using std::map;
+using std::vector;
 
-size_t const MAX_READ_SIZE = 4096;
+namespace langdetect {
+    unsigned int const Detector::MAX_READ_SIZE = 12288;
+    int const Detector::DEFAULT_TRIAL = 7;
+    double const Detector::DEFAULT_ALPHA = 0.5;
+    double const Detector::ALPHA_WIDTH = 0.05;
+    int const Detector::ITERATION_LIMIT = 1000;
+    double const Detector::PROB_THRESHOLD = 0.1;
+    double const Detector::CONV_THRESHOLD = 0.99999;
+    int const Detector::BASE_FREQ = 10000;
+    std::string const Detector::UNKNOWN_LANG = "unknown";
 
-void message(char const *path, char const *msg) {
-    std::cout << path << ": " << msg << std::endl;
+    Detector::Detector() : trial_(DEFAULT_TRIAL), alpha_(DEFAULT_ALPHA) {}
+
+    Detected Detector::detect(char const *data, unsigned int const &length) {
+        if(length == 0) return LANG_FAIL_EMPTY;
+        NgramStorage& storage = NgramStorage::instance();
+        size_t rlen = length;
+        if(rlen > MAX_READ_SIZE) rlen = MAX_READ_SIZE;
+        CodeSequence codesequence(data, rlen);
+        vector<string> grams = codesequence.tongram();
+        if(grams.empty()) return Detected(UNKNOWN_LANG, 0.0);
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::normal_distribution<> gaussian;
+        std::uniform_int_distribution<> uniform_idx(0, grams.size() - 1);
+        vector<double> finalscores(storage.langlist().size(), 0);
+        for(int i = 0; i < trial_; ++i) {
+            vector<double> scores(storage.langlist().size(), 1.0 / storage.langlist().size());  // priorはuniformに固定する
+            double alpha = alpha_ + gaussian(gen) * ALPHA_WIDTH;
+            for(int j = 0;;) {
+                // 乱数からどのn-gramを使うかを決定する
+                size_t tgt_idx = uniform_idx(gen);
+                string ngram = grams[tgt_idx];
+                if(ngram.empty()) continue;
+                update_scores_(ngram, scores, alpha);
+                if( j % 5 == 0) {
+                    if(normalize_(scores) > CONV_THRESHOLD || j >= ITERATION_LIMIT) break;
+                }
+                j += 1; // count up
+            }
+            for(size_t j = 0; j < finalscores.size(); ++j) {
+                finalscores[j] += scores[j] / trial_;
+            }
+        }
+
+        vector<Detected> detecteds(storage.langlist().size());
+        for(size_t i = 0; i < finalscores.size(); ++i) {
+            detecteds[i].name(storage.langlist()[i]);
+            detecteds[i].score(finalscores[i]);
+        }
+        // for(auto &d : detecteds) {
+        //     std::cout << d.name() << ", " << d.score() << std::endl;
+        // }
+        // sort
+        std::sort(detecteds.begin(), detecteds.end());
+        return detecteds.back();
+    }
+
+    void Detector::update_scores_(string const &ngram, vector<double> &scores, double const &alpha) {
+        NgramStorage& storage = NgramStorage::instance();
+        if(storage.has(ngram)) {
+            double weight = alpha / BASE_FREQ;
+            auto plist = storage.get(ngram);
+            // for(size_t i = 0; i < ngram.length(); ++i) std::cout << std::hex << (int)*(uint8_t *)(ngram.data() + i) << " ";
+            // std::cout << std::endl;
+            for(size_t i = 0; i < plist.size(); ++i) {
+                // std::cout << " [" << storage.langlist()[i] << "]= " << plist[i];
+                scores[i] *= weight + plist[i];
+            }
+            // std::cout << std::endl;
+        }
+    }
+
+    double Detector::normalize_(vector<double> &scores) {
+        double total = 0.0;
+        double maxp = 0.0;
+        for(size_t i = 0; i < scores.size(); ++i) total += scores[i];
+        for(size_t i = 0; i < scores.size(); ++i) {
+            double norm = scores[i] / total;
+            if(maxp < norm) maxp = norm;
+            scores[i] = norm;
+        }
+        return maxp;
+    }
+
+
+    Detected::Detected(string const &name, double const &score)
+    : name_(name), score_(score) {}
+
+    Detected::Detected() {};
+
+    bool Detected::operator==(Detected const &other) const {
+        return score_ == other.score_;
+    }
+    bool Detected::operator!=(Detected const &other) const {
+        return score_ != other.score_;
+    }
+    bool Detected::operator<(Detected const &other) const {
+        return score_ < other.score_;
+    }
+    bool Detected::operator>(Detected const &other) const {
+        return score_ > other.score_;
+    }
+    bool Detected::operator<=(Detected const &other) const {
+        return score_ <= other.score_;
+    }
+    bool Detected::operator>=(Detected const &other) const {
+        return score_ >= other.score_;
+    }
+
+    void Detected::name(std::string const &name) {name_ = name;}
+    std::string const & Detected::name() const {return name_;}
+    void Detected::score(double const &score) {score_ = score;}
+    double const & Detected::score() const {return score_;}
 }
 
+#include "./langdetect_c.h"
 
-/// 言語判定を行います
-void detect(char const *path) {
-    Detector detector;
-    ifstream ifs(path);
-    if(!ifs.is_open()) {
-        message(path, "failed to open");
+LANGDETECT_LANGS langdetect_detect(char const *data, unsigned int const &length) {
+    double score = 0.0;
+    return langdetect_detect_with_score(data, length, score);
+}
+
+LANGDETECT_LANGS langdetect_detect_with_score(char const *data, unsigned int const &length, double &score) {
+    langdetect::Detector detector;
+    langdetect::Detected result = detector.detect(data, length);
+    score = result.score();
+    string name = result.name();
+    langdetect::NgramStorage& storage = langdetect::NgramStorage::instance();
+    std::vector<std::string>::const_iterator itr = std::find(storage.langlist().begin(), storage.langlist().end(), name);
+    if(itr != storage.langlist().end()) {
+        return static_cast<LANGDETECT_LANGS>(itr - storage.langlist().begin());
+    } else if(name == "empty") {
+        return LANGDETECT_EMPTY;
+    } else if(name == "unknown") {
+        return LANGDETECT_UNKNOWN;
     } else {
-        char buf[MAX_READ_SIZE];  // read max 4k bytes
-        ifs.read(buf, MAX_READ_SIZE);
-        std::streamsize readcount = ifs.gcount();
-        langdetect::Detected result = detector.detect(buf, readcount);
-        message(path, result.name().c_str());
+        return LANGDETECT_ERROR;
     }
-}
-
-
-int main(int argc, char const *argv[]) {
-    for(int i = 1; i < argc; ++i) {
-        detect(argv[i]);
-    }
-    return 0;
 }
